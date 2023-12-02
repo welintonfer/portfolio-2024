8ef7a2dc49e321d4c654e745376b2c89d61bf546
Welinton Fernadnes <portfolio.fernandes@gmail.com>
2023-12-02 20:07:15 +0000

    Installing Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+// npm i react-ga4 Google analytics
+import ReactGA from "react-ga4";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-T26QG4V20E");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
