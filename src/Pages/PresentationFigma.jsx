98c7390019900175d0c1f41f7aae7ad4b4291fc9
Welinton Fernadnes <portfolio.fernandes@gmail.com>
2023-12-18 12:49:29 +0000

    Figma presentation test 6

--- src/Pages/PresentationFigma.jsx
+++ src/Pages/PresentationFigma.jsx
@@ -2,7 +2,7 @@ import "./css/style.css";
 
 export default function PresentationFigma() {
   return (
-    <>
+    <div style={{ width: "100%", height: "100vh" }}>
       <iframe
         style={{
           width: "100%",
@@ -12,6 +12,6 @@ export default function PresentationFigma() {
         src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FPJpSM9USbtZxJ8QVMMFOO0%2FHabitus-Health%3Fpage-id%3D43%253A3199%26type%3Ddesign%26node-id%3D43-3200%26viewport%3D1087%252C5700%252C0.45%26t%3DumavSh08SR5Ieug8-1%26scaling%3Dscale-down%26mode%3Ddesign"
         allowFullScreen
       ></iframe>
-    </>
+    </div>
   );
 }
