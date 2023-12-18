91236eef1d6e86669f0dce375437f82dcdf12032
Welinton Fernadnes <portfolio.fernandes@gmail.com>
2023-12-18 12:44:05 +0000

    Figma presentation test 4

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -384,16 +384,6 @@ export default function AboutMe() {
         </div>
       </section>
 
-      <iframe
-        style={{
-          width: "100%",
-          height: "100%",
-          border: "1px solid rgba(0, 0, 0, 0.1)",
-        }}
-        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FPJpSM9USbtZxJ8QVMMFOO0%2FHabitus-Health%3Fpage-id%3D43%253A3199%26type%3Ddesign%26node-id%3D43-3200%26viewport%3D1087%252C5700%252C0.45%26t%3DumavSh08SR5Ieug8-1%26scaling%3Dscale-down%26mode%3Ddesign"
-        allowFullScreen
-      ></iframe>
-
       <section className="case-study-single__design-process">
         <h2 className="h2">Profile</h2>
         <p>
--- src/Pages/PresentationFigma.jsx
+++ src/Pages/PresentationFigma.jsx
@@ -3,17 +3,15 @@ import "./css/style.css";
 export default function PresentationFigma() {
   return (
     <>
-      <div style={{ width: "100%", height: "100vh" }}>
-        <iframe
-          style={{
-            width: "100%",
-            height: "100%",
-            border: "1px solid rgba(0, 0, 0, 0.1)",
-          }}
-          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FPJpSM9USbtZxJ8QVMMFOO0%2FHabitus-Health%3Fpage-id%3D43%253A3199%26type%3Ddesign%26node-id%3D43-3200%26viewport%3D1087%252C5700%252C0.45%26t%3DumavSh08SR5Ieug8-1%26scaling%3Dscale-down%26mode%3Ddesign"
-          allowFullScreen
-        ></iframe>
-      </div>
+      <iframe
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "1px solid rgba(0, 0, 0, 0.1)",
+        }}
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FPJpSM9USbtZxJ8QVMMFOO0%2FHabitus-Health%3Fpage-id%3D43%253A3199%26type%3Ddesign%26node-id%3D43-3200%26viewport%3D1087%252C5700%252C0.45%26t%3DumavSh08SR5Ieug8-1%26scaling%3Dscale-down%26mode%3Ddesign"
+        allowFullScreen
+      ></iframe>
     </>
   );
 }
