581c0bd91e2129b1e3c7b4d8c0aa80297a184aa3
Welinton Fernadnes <portfolio.fernandes@gmail.com>
2024-10-24 11:16:26 -0300

    portfolio update 4

--- src/Pages/Projects/Europcar.jsx
+++ src/Pages/Projects/Europcar.jsx
@@ -209,3 +209,5 @@ export default function Europcar() {
     </motion.div>
   );
 }
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,41 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// // npm i react-ga4 Google analytics
+// import ReactGA from "react-ga4";
+
+// ReactGA.initialize("G-T26QG4V20E");
+
+// ReactGA.send({
+//   hitType: "pageview",
+//   page: window.location.pathname,
+// });
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// npm i react-ga4 Google analytics
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-T26QG4V20E");
+// Inicializar apenas se estiver em produção
+if (process.env.NODE_ENV === "production") {
+  ReactGA.initialize("G-T26QG4V20E");
 
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+    anonymizeIp: true,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
