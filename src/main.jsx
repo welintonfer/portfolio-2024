919a6d29881b2023a3303334ca4885377f65394a
Welinton Fernadnes <portfolio.fernandes@gmail.com>
2024-10-24 11:18:38 -0300

    portfolio update 5

--- src/main.jsx
+++ src/main.jsx
@@ -20,25 +20,48 @@
 
 
 
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ReactGA from "react-ga4";
 
-// Inicializar apenas se estiver em produção
-if (process.env.NODE_ENV === "production") {
-  ReactGA.initialize("G-T26QG4V20E");
+// Inicializa o Google Analytics se não for seu IP
+async function initializeAnalytics() {
+  try {
+    const response = await fetch("https://api64.ipify.org?format=json");
+    const data = await response.json();
+    const userIP = data.ip;
 
-  ReactGA.send({
-    hitType: "pageview",
-    page: window.location.pathname,
-    anonymizeIp: true,
-  });
+    const blockedIPs = ["189.18.207.112", "2001:4860:7:303::a0"]; // Seus IPs
+
+    if (!blockedIPs.includes(userIP)) {
+      ReactGA.initialize("G-T26QG4V20E");
+      ReactGA.send({
+        hitType: "pageview",
+        page: window.location.pathname,
+        anonymizeIp: true, // Anonimiza mesmo para outros usuários
+      });
+      console.log("Google Analytics inicializado.");
+    } else {
+      console.log("Rastreamento bloqueado para o seu IP.");
+    }
+  } catch (error) {
+    console.error("Erro ao buscar o IP do usuário:", error);
+  }
+}
+
+function Main() {
+  useEffect(() => {
+    initializeAnalytics();
+  }, []);
+
+  return <App />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
+
